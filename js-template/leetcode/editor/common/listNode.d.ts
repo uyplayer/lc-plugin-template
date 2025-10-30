14831d3eb2e8f488999e8296287cb88f3097c80f
Your Name <you@example.com>
2025-10-30 12:05:43 +0800

    更新 文件

--- js-template/leetcode/editor/common/listNode.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null);
-    static print(head: ListNode | null): void;
-    static createHead(nums: number[]): ListNode | null;
-}
--- js-template/leetcode/editor/common/treeNode.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TreeNode {
-    val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null);
-    static createRoot(arr: (number | null)[]): TreeNode | null;
-}
