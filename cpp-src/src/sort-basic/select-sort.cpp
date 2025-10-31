1fec6d8efdfe4821361f92f1f7553680d4403fac
Your Name <you@example.com>
2025-10-31 10:39:35 +0800

    update select sort

--- cpp-src/src/sort-basic/select-sort.cpp
+++ cpp-src/src/sort-basic/select-sort.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 
-void select_sort(const std::vector<int> &array);
+void select_sort(std::vector<int> &array);
 
 int main()
 {
@@ -13,6 +13,17 @@ int main()
                            3, 4, 8, 19, 6, 3, 58, 4, 1, 4, 64, 64, 64, 651, 31, 98, 98, 3031, 3};
     select_sort(array);
 
+    std::cout << "快速排序后数组 \n";
+    for (std::size_t i = 0; i < array.size(); ++i)
+    {
+        std::cout << array[i];
+        if (i + 1 < array.size())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << '\n';
+
     return 0;
 }
 
@@ -21,7 +32,7 @@ int main()
  *
  * @param array 要排序的数组
  */
-void select_sort(const std::vector<int> &array)
+void select_sort(std::vector<int> &array)
 {
     std::cout << "原始数组 \n";
     for (std::size_t i = 0; i < array.size(); ++i)
@@ -34,12 +45,25 @@ void select_sort(const std::vector<int> &array)
     }
     std::cout << '\n';
 
-    auto array_size = array.size();
-    auto current_index = 0;
+    std::size_t array_size = array.size();
+    std::size_t current_index = 0;
     while (current_index < array_size)
     {
-        for (int i = current_index; i < array_size; i++)
+
+        // 记录当前的循环的最小元素的index
+        std::size_t min_num_index = current_index;
+        for (std::size_t i = current_index + 1; i < array_size; i++)
         {
+
+            if (array[i] < array[min_num_index])
+            {
+                min_num_index = i;
+            }
         }
+        // 这一轮里找到最小值
+        auto tmp = array[current_index];
+        array[current_index] = array[min_num_index];
+        array[min_num_index] = tmp;
+        current_index++;
     }
 }
