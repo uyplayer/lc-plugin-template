9f4b5c5a109cde4716a17d18e222ea6794f4e717
uyplayer <uyplayer@outlook.com>
2025-12-31 12:08:52 +0800

    ``` fix(vscode): 修复工作区路径配置
    修复了 labuladong-leetcode.workspaceFolder 配置项中的路径，
    将 Linux 风格的路径 /home/uyplayer 修改为 macOS 风格的路径 /Users/uyplayer，
    以适配不同的操作系统环境。
    ```

---
+++ 837.new-21-game.c
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=837 lang=c
+ * @lcpr version=30305
+ *
+ * [837] 新 21 点
+ */
+
+// @lc code=start
+double new21Game(int n, int k, int maxPts) {
+    
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 10\n1\n10\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 6\n1\n10\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 21\n17\n10\n
+// @lcpr case=end
+
+ */
+
---
+++ cpp-template/leetcode/editor/cn/global-and-local-inversions.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=775 lang=cpp
+ * @lcpr version=30305
+ *
+ * [775] 全局倒置与局部倒置
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isIdealPermutation(vector<int> &nums)
+    {
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,0,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,0]\n
+// @lcpr case=end
+
+ */
