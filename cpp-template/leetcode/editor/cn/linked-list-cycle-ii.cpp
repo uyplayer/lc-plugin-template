c1ec7bcefad5f334e3b6e2768690576eb802e1a4
uyplayer <uyplayer@outlook.com>
2026-01-02 15:21:40 +0800

    ``` feat: 添加链表相关算法题解
    添加了两个链表相关的LeetCode题解：
    - intersection-of-two-linked-lists.cpp: 实现相交链表查找算法
    - linked-list-cycle-ii.cpp: 实现环形链表II检测算法
    ```

---
+++ cpp-template/leetcode/editor/cn/linked-list-cycle-ii.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ * @lcpr version=30305
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        ListNode *slow = head, *fast = head;
+         while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) break;
+
+        }
+
+        // 上面的代码类似 hasCycle 函数
+        if (fast == nullptr || fast->next == nullptr) {
+            // fast 遇到空指针说明没有环
+            return nullptr;
+        }
+
+        // 重新指向头结点
+        slow = head;
+
+        // 快慢指针同步前进，相交点就是环起点
+        while (slow != fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+
+
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
+
