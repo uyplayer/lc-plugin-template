c6ec52bfd001887a117e2793ee19040491f35ee3
uyplayer <uyplayer@outlook.com>
2026-02-03 17:14:04 +0800

    ``` feat(test): 添加向量遍历功能实现
    实现了递归方式的向量遍历算法，包含traverse函数用于递归遍历数组元素，
    vector_traverse函数作为主入口点并提供测试用例，验证数组[1,2,3,4,5,6,7,8,9,63,234,43,554,65,7,3244,24,23]
    的逆序输出功能。
    ```

--- test/vector_travers.py
+++ test/vector_travers.py
@@ -1,5 +1,17 @@
 
+def traverse(array:list,index:int):
+
+    if len(array) == index:
+        return 
+
+    traverse(array,index+1)
+    print(array[index])
+
+def vector_traverse():
+
+    array = [1,2,3,4,5,6,7,8,9,63,234,43,554,65,7,3244,24,23]
+    traverse(array,0)
 
 
-
-def trave
+if __name__ == "__main__":
+    vector_traverse()
